9fe6041e25965129274d2e76a9a292b0a27d5ffe
Juan Duzac <duzacj@gmail.com>
2021-06-29 21:03:56 -0300

    page is now refreshing after registering a food POST

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,7 +1,12 @@
-function registerFood(){
+function registerFood(date){
     var foodAnchor = document.getElementsByClassName("list-group-item active")[0];
     var amount = document.getElementById("amount-input");
     var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() { // listen for state changes
+        if (xhr.readyState == 4 && xhr.status == 200) { // when completed we can move away
+            window.location = "/food/registration";
+        }
+    }
     xhr.open("POST", "/food/registration", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
