8582304c1c234156c478a1e6ed5d2352ea0dfbdd
Thomas Broderick <thomasbroderick99@gmail.com>
2021-07-21 00:03:11 -0300

    Created Exercise Page

--- src/main/groovy/com/fitness/tracker/exercise/model/Exercise.groovy
+++ src/main/groovy/com/fitness/tracker/exercise/model/Exercise.groovy
@@ -24,7 +24,7 @@ class Exercise {
     String name
 
     @NotNull
-    BigDecimal caloriesBurned
+    BigDecimal caloriesBurnedPerMinute
 
     @NotNull
     Type type
@@ -37,6 +37,10 @@ class Exercise {
     boolean isStrength() {
         if (type == Type.STRENGTH)
             true
+    }
+
+    BigDecimal getTotalCaloriesBurned(BigDecimal time) {
+        this.caloriesBurnedPerMinute * time
     }
 
 }
--- src/main/groovy/com/fitness/tracker/exercise/model/ExerciseRegistration.groovy
+++ src/main/groovy/com/fitness/tracker/exercise/model/ExerciseRegistration.groovy
@@ -2,7 +2,10 @@ package com.fitness.tracker.exercise.model
 
 import com.fitness.tracker.person.model.Registration
 
+import javax.persistence.CascadeType
 import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 import javax.persistence.Table
 import javax.validation.constraints.NotNull
 
@@ -11,9 +14,14 @@ import javax.validation.constraints.NotNull
 class ExerciseRegistration extends Registration{
 
     @NotNull
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "exercise_id", referencedColumnName = "id")
     Exercise exercise
 
     @NotNull
     BigDecimal time //create class duration with HH:MM:SS or import from somewhere
+
+    @NotNull
+    BigDecimal weight
 
 }
