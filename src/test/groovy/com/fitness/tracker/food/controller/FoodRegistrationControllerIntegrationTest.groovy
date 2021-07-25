9a77a466a60527f7dc4f8886c6ec8868dc948fe8
Juan Duzac <duzacj@gmail.com>
2021-07-25 20:55:01 -0300

    Person services is now spy on the integrationTest instead of a mock

--- src/test/groovy/com/fitness/tracker/food/controller/FoodRegistrationControllerIntegrationTest.groovy
+++ src/test/groovy/com/fitness/tracker/food/controller/FoodRegistrationControllerIntegrationTest.groovy
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -21,6 +21,8 @@ import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequ
 import org.springframework.http.MediaType
 
 import java.time.LocalDate
+
+import static org.mockito.Mockito.doReturn
 import static org.mockito.Mockito.when
 
 
@@ -31,7 +33,7 @@ class FoodRegistrationControllerIntegrationTest {
     @Autowired
     MockMvc mockMvc
 
-    @MockBean
+    @SpyBean
     PersonService personService
 
     @Autowired
@@ -71,7 +73,8 @@ class FoodRegistrationControllerIntegrationTest {
         personUsedToTest = person
         personRepository.save(person)
 
-        when(personService.getPrincipal()).thenReturn(person)
+        doReturn(person).when(personService).getPrincipal()
+        //when(personService.getPrincipal()).thenReturn(person)
     }
 
     @Test
